1850ec14446b1fbd9592b3a13827b67ce5eab0b3
dragan.jovicic <dragan.jovicic@walter.ba>
2018-11-19 20:34:52 +0100

    Set up for Heroku

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-var {mongoose} = require('./db/mongoose');
+var {mongoose, } = require('./db/mongoose');
 var {User} = require('./models/user');
 var {ToDo} = require('./models/todo');
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 
-app.listen(3000, () => {
-    console.log('Started on 3000');
+app.listen(port, () => {
+    console.log(`Started up at port ${port}` );
 });
 
 app.use(bodyParser.json());
@@ -27,3 +29,36 @@ app.post('/todos', (req, res) => {
         res.send(e);
     })
 })
+
+app.get('/todos', (req, res) => {
+    ToDo.find().then((todos) => {
+        res.status(200);
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+})
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    ToDo.findById(id).then((todo) => {
+       if(todo) {
+        res.status(200);
+        res.send({todo});
+       } else {
+           res.status(404);
+           res.send();
+       }
+    }, (e) => {
+        res.status(400);
+        res.send(e);
+    })
+});
+
+
+module.exports = {app};
