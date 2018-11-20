4df77c76ffdeb16c90dbf8f8ea8318b03f42ce01
dragan.jovicic <dragan.jovicic@walter.ba>
2018-11-20 10:42:49 +0100

    POST AND DELETE ROUTE

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
-var {mongoose, } = require('./db/mongoose');
-var {User} = require('./models/user');
-var {ToDo} = require('./models/todo');
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectId} = require('mongodb');
+const env = process.env.NODE_ENV || 'development';
+
+if(env === 'development') {
+    process.env.PORT ===3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp';
+
+} else if( env === 'test') {
+    process.env.PORT ===3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoAppTest';
+}
+
+const {mongoose, } = require('./db/mongoose');
+const {User} = require('./models/user');
+const {ToDo} = require('./models/todo');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
+const _  = require('lodash');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 app.listen(port, () => {
@@ -59,6 +71,36 @@ app.get('/todos/:id', (req, res) => {
         res.send(e);
     })
 });
+
+app.delete(`/todos/:id`, (req, res) => {
+    ToDo.findByIdAndRemove(req.params.id).then((todo) => {
+        res.status(200);
+        res.send({todo});
+    }, (e) => {
+        res.status(400);
+        res.send(e);
+    })
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectId.isValid(id)) {
+        return res.status(400).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    ToDo.findByIdAndUpdate(id,{$set: body}, {new: true}).then((todo) => {
+        res.status(200).send({todo});
+    }).catch((e) => res.status(400).send(e));
+})
 
 
 module.exports = {app};
